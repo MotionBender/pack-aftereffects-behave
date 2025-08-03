f616ad84b548455c4246a58a4f8a8bf6a8da4d7d
lucasolasz <olaszlucas072@gmail.com>
2025-08-03 15:08:25 -0300

    Inclui prop de href no botao

--- app/_components/button-comprar.tsx
+++ app/_components/button-comprar.tsx
@@ -1,13 +1,18 @@
 import { ReactNode } from "react";
+import Link from "next/link";
 
 type ButtonComprarProps = {
   children: ReactNode;
+  href: string;
 };
 
-export function ButtonComprar({ children }: ButtonComprarProps) {
+export function ButtonComprar({ children, href }: ButtonComprarProps) {
   return (
-    <button className="px-20 py-2 cursor-pointer bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 text-white text-lg font-semibold rounded-full border-none shadow-none transition duration-300 hover:from-pink-600 hover:via-purple-600 hover:to-blue-600 active:scale-95 active:shadow-inner">
+    <Link
+      href={href}
+      className="px-20 py-2 inline-block cursor-pointer bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 text-white text-lg font-semibold rounded-full border-none shadow-none transition duration-300 hover:from-pink-600 hover:via-purple-600 hover:to-blue-600 active:scale-95 active:shadow-inner"
+    >
       {children}
-    </button>
+    </Link>
   );
 }
